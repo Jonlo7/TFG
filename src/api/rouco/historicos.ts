f599f4a5c735bb63668f25574eedc49919099565
Jon Lopez <jonlopez@opendeusto.es>
2024-06-01 16:43:56 +0200

    fix Mostrar datos de las tablas

--- src/api/rouco/historicos.ts
+++ src/api/rouco/historicos.ts
@@ -8,7 +8,11 @@ router.get(
     async (req: Request, res: Response, next: NextFunction) => {
         const trabajador = req.session.trabajador;
         let isAdmin: boolean = trabajador?.id_Cargo === '5262b2a3-328e-4843-bede-cd02a21e5d49';
-        res.render("historicos", { layout: "index", isAdmin: isAdmin});
+        const historialLotes = (await Lote.findAll()).map(lote => lote.toJSON());
+        const historialAlarmas = (await HistorialAlarmas.findAll()).map(historialAlarma => historialAlarma.toJSON());
+        console.log(historialAlarmas);
+
+        res.render("historicos", { layout: "index", isAdmin: isAdmin, historialAlarmas, historialLotes});
     },
 );
 
