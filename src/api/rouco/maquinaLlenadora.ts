f4a6db908588b95c6d4dd56baef58193db4ca02d
Jon Lopez <jonlopez@opendeusto.es>
2024-06-02 19:30:58 +0200

    feat Full funcional

--- src/api/rouco/maquinaLlenadora.ts
+++ src/api/rouco/maquinaLlenadora.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { TiempoReal,Botones} from '../../schemas';
+import { set } from 'shelljs';
 
 const router = Router();
 
@@ -33,29 +34,32 @@ router.post(
     "/botones",
     async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const { bWEBMarcha, bWEBAspiradora, bWEBCinta } = req.body;
-
-            // create or update
+            const { bWEBMarcha, bWEBAspiradora, bWEBCinta,bWEBCrearLote,bWEBEmergencia,bWEBModoAuto,bWEBMotorLlenadora,bWEBParo,bWEBResetCounterEntrada,bWEBResetCounterSalida,bWEBTerminarLote,bWEBValvula } = req.body;
             if (bWEBMarcha) {
                 await Botones.upsert({
                     id_Boton: 'bWEBMarcha',
-                    valor: bWEBMarcha
+                    valor: Boolean(bWEBMarcha)
                 });
-
                 setTimeout(() => {
                     Botones.upsert({
                         id_Boton: 'bWEBMarcha',
                         valor: false
                     });
-                }, 1000);
+                }, 500);
             }
             
 
-            if (bWEBAspiradora) {
+           if (bWEBAspiradora) {
                 await Botones.upsert({
                     id_Boton: 'bWEBAspiradora',
                     valor: bWEBAspiradora
                 });
+                setTimeout(() => {
+                    Botones.upsert({
+                        id_Boton: 'bWEBAspiradora',
+                        valor: false
+                    });
+                }, 500);
             }
 
             if (bWEBCinta) {
@@ -63,11 +67,124 @@ router.post(
                     id_Boton: 'bWEBCinta',
                     valor: bWEBCinta
                 });
+                setTimeout(() => {
+                    Botones.upsert({
+                        id_Boton: 'bWEBCinta',
+                        valor: false
+                    });
+                }, 500);
             }
 
-            //...
+            if (bWEBCrearLote) {
+                await Botones.upsert({
+                    id_Boton: 'bWEBCrearLote',
+                    valor: bWEBCrearLote
+                });
+                setTimeout(() => {
+                    Botones.upsert({
+                        id_Boton: 'bWEBCrearLote',
+                        valor: false
+                    });
+                }, 500);
+            }
 
+            if (bWEBEmergencia) {
+                await Botones.upsert({
+                    id_Boton: 'bWEBEmergencia',
+                    valor: bWEBEmergencia
+                });
+                setTimeout(() => {
+                    Botones.upsert({
+                        id_Boton: 'bWEBEmergencia',
+                        valor: false
+                    });
+                }, 500);
+            }
 
+            if (bWEBModoAuto) {
+                await Botones.upsert({
+                    id_Boton: 'bWEBModoAuto',
+                    valor: bWEBModoAuto
+                });
+            }
+
+            if (bWEBMotorLlenadora) {
+                await Botones.upsert({
+                    id_Boton: 'bWEBMotorLlenadora',
+                    valor: bWEBMotorLlenadora
+                });
+                setTimeout(() => {
+                    Botones.upsert({
+                        id_Boton: 'bWEBMotorLlenadora',
+                        valor: false
+                    });
+                }, 500);
+            }
+
+            if (bWEBParo) {
+                await Botones.upsert({
+                    id_Boton: 'bWEBParo',
+                    valor: bWEBParo
+                });
+                setTimeout(() => {
+                    Botones.upsert({
+                        id_Boton: 'bWEBParo',
+                        valor: false
+                    });
+                }, 500);
+            }
+
+            if (bWEBResetCounterEntrada) {
+                await Botones.upsert({
+                    id_Boton: 'bWEBResetCounterEntrada',
+                    valor: bWEBResetCounterEntrada
+                });
+                setTimeout(() => {
+                    Botones.upsert({
+                        id_Boton: 'bWEBResetCounterEntrada',
+                        valor: false
+                    });
+                }, 500);
+            }
+
+            if (bWEBResetCounterSalida) {
+                await Botones.upsert({
+                    id_Boton: 'bWEBResetCounterSalida',
+                    valor: bWEBResetCounterSalida
+                });
+                setTimeout(() => {
+                    Botones.upsert({
+                        id_Boton: 'bWEBResetCounterSalida',
+                        valor: false
+                    });
+                }, 500);
+            }
+
+            if (bWEBTerminarLote) {
+                await Botones.upsert({
+                    id_Boton: 'bWEBTerminarLote',
+                    valor: bWEBTerminarLote
+                });
+                setTimeout(() => {
+                    Botones.upsert({
+                        id_Boton: 'bWEBTerminarLote',
+                        valor: false
+                    });
+                }, 500);
+            }
+
+            if (bWEBValvula) {
+                await Botones.upsert({
+                    id_Boton: 'bWEBValvula',
+                    valor: bWEBValvula
+                });
+                setTimeout(() => {
+                    Botones.upsert({
+                        id_Boton: 'bWEBValvula',
+                        valor: false
+                    });
+                }, 500);
+            } 
             res.json({ message: 'Botones actualizados' });
         } catch (error) {
             next(error);
