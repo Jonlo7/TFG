73efed4545c1b8cf30eb6b18f29cc86eb50a3d0f
Jon Lopez <jonlopez@opendeusto.es>
2024-06-03 01:08:17 +0200

    feat Añadir y eliminar trabajador

--- src/api/rouco/admin.ts
+++ src/api/rouco/admin.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { Trabajador,Cargo} from '../../schemas';
+import { APIError } from '../error/APIError'; 
 
 const router = Router();
 
@@ -7,9 +8,31 @@ router.get(
     "/admin",
     async (req: Request, res: Response, next: NextFunction) => {   
         const trabajador = req.session.trabajador;
-        let isAdmin: boolean = trabajador?.id_Cargo === '5262b2a3-328e-4843-bede-cd02a21e5d49';         
-        res.render("admin", { layout: "index", isAdmin: isAdmin});
+        let isAdmin: boolean = trabajador?.id_Cargo === '5262b2a3-328e-4843-bede-cd02a21e5d49';      
+        const trabajadores = (await Trabajador.findAll()).map(trabajador => trabajador.toJSON());   
+        const cargos = (await Cargo.findAll()).map(cargo => cargo.toJSON());
+        res.render("admin", { layout: "index", isAdmin: isAdmin,trabajadores,cargos});
     },
 );
+//verify if user already created, if not create user
+router.post(
+    "/add-trabajador",
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { nombre, apellido, email, password, id_Cargo } = req.body;
+            const trabajador = await Trabajador.findOne({
+                where: {
+                    email,
+                },
+            });
+            if (trabajador) throw new APIError("Ya existe", true);
+            await Trabajador.create({nombre, apellido, email, passwordHash: password, id_Cargo});
+            res.json({ success: true });
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
 
 export default router;
