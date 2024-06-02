8aed8441a8b97fb20809af561edd7684ae1eaea1
Jon Lopez <jonlopez@opendeusto.es>
2024-06-02 22:30:45 +0200

    feat Completamente funcional

--- src/api/rouco/historicos.ts
+++ src/api/rouco/historicos.ts
@@ -10,8 +10,6 @@ router.get(
         let isAdmin: boolean = trabajador?.id_Cargo === '5262b2a3-328e-4843-bede-cd02a21e5d49';
         const historialLotes = (await Lote.findAll()).map(lote => lote.toJSON());
         const historialAlarmas = (await HistorialAlarmas.findAll()).map(historialAlarma => historialAlarma.toJSON());
-        console.log(historialAlarmas);
-
         res.render("historicos", { layout: "index", isAdmin: isAdmin, historialAlarmas, historialLotes});
     },
 );
